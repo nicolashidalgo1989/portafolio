4606bc3ceb65928e17a6e02c6fffb69c012b41bb
nicolas.hidalgo.1989 <nicolas.hidalgo.1989@gmail.com>
2018-05-23 18:25:53 -0400

    background, iconos, secciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,23 @@
 //variables
 const   btnMenu = document.querySelectorAll('.btn-menu')[0],
-        btnSearch = document.querySelectorAll('.btn-search')[0];      
+        btnSearch = document.querySelectorAll('.btn-search')[0],
+        btnScrollDown = document.querySelectorAll('.btn-scroll-down')[0],
+        header = document.querySelectorAll('header')[0],
+        menu = document.querySelectorAll('.menu')[0];
 //listeners
 btnMenu.addEventListener('click', openMenu);
 btnSearch.addEventListener('click', openSearch);
+btnScrollDown.addEventListener('click', scrollDown); 
 //functions  
 function openMenu(e){  
     btnMenu.children[0].classList.toggle('fa-bars');
     btnMenu.children[0].classList.toggle('fa-times'); 
-    btnMenu.parentElement.parentElement.parentElement.parentElement.classList.toggle('open');
-    btnMenu.children[1].classList.toggle('hidden');
+    header.classList.toggle('open');
+    menu.classList.toggle('hidden');
 }
 function openSearch(e){
     btnSearch.classList.toggle('open');
+} 
+function scrollDown(){
+    console.log('hola')
 }
