178b79b38729806e2aa1db5b647e34b7a820d0e6
nicolas.hidalgo.1989 <nicolas.hidalgo.1989@gmail.com>
2018-05-24 18:40:12 -0400

    formulario contacto y formulario search

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,19 @@
 //variables
 const   btnMenu = document.querySelectorAll('.btn-menu')[0],
-        btnSearch = document.querySelectorAll('.btn-search')[0],
-        btnScrollDown = document.querySelectorAll('.btn-scroll-down')[0],
+        btnSearch = document.querySelectorAll('.btn-search')[0], 
         header = document.querySelectorAll('header')[0],
-        menu = document.querySelectorAll('.menu')[0];
+        menu = document.querySelectorAll('.menu')[0],
+        input = document.querySelectorAll('input'),
+        textArea = document.querySelectorAll('textarea')[0];
 //listeners
 btnMenu.addEventListener('click', openMenu);
-btnSearch.addEventListener('click', openSearch);
-btnScrollDown.addEventListener('click', scrollDown); 
+btnSearch.addEventListener('click', openSearch); 
+for(i=0;input.length>i;i++){ 
+    input[i].addEventListener('click', activeLabel);
+    input[i].addEventListener('blur', outInput);
+}
+textArea.addEventListener('click', activeLabel);
+textArea.addEventListener('blur', outInput);
 //functions  
 function openMenu(e){  
     btnMenu.children[0].classList.toggle('fa-bars');
@@ -17,8 +23,25 @@ function openMenu(e){
     document.querySelector('body').classList.toggle('overflow-hidden');
 }
 function openSearch(e){
-    btnSearch.classList.toggle('open');
+    console.log(e.target);
+    let clase = e.target.classList;
+    if(!e.target.parentElement.classList.contains('open')){
+        btnSearch.classList.add('open'); 
+    }else if(clase.contains('clear-search')){
+        btnSearch.classList.remove('open');  
+    }
+}  
+function activeLabel(e){ 
+    e.target.parentElement.children[1].classList.add('active');   
 } 
-function scrollDown(){
-    console.log('hola')
-}
+function outInput(e){ 
+    let errorMsg = e.target.parentElement.children[2];
+    let id = e.target.id;
+    if(e.target.value.length === 0){
+        e.target.parentElement.children[1].classList.remove('active');
+        errorMsg.innerHTML = `escribe un ${id} válido`;
+        errorMsg.classList.add('show'); 
+    }else{
+        errorMsg.classList.remove('show'); 
+    }
+} 
