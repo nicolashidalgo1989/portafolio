c9db12b787d609c2d5009336287e2155222d0e65
nicolas.hidalgo.1989 <nicolas.hidalgo.1989@gmail.com>
2018-05-30 17:58:20 -0400

    filtros en portafolio con js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,8 @@ const   btnMenu = document.querySelector('.btn-menu'),
         btnSearch = document.querySelectorAll('.btn-search')[0], 
         inputSearch = document.querySelectorAll('.input-search')[0],
         header = document.querySelectorAll('header')[0], 
-        input = document.querySelectorAll('input'),
-        textArea = document.querySelectorAll('textarea')[0],
+        input = document.querySelectorAll('.input'),
+        textArea = document.querySelector('.textarea'),
         body = document.querySelector('body'), 
         linksMenu = document.querySelectorAll('.menu a'),
         elHeader = document.querySelector('.header .container .row'),
@@ -18,16 +18,18 @@ btnMenu.appendChild(line);
 
 //listeners 
 document.addEventListener('DOMContentLoaded', createMenuHeader);
-btnMenu.addEventListener('click', openMenu);
-btnSearch.addEventListener('click', openSearch); 
-inputSearch.addEventListener('click', clearSearch); 
-for(i=0;input.length>i;i++){ 
-    input[i].addEventListener('click', activeLabel);
-    input[i].addEventListener('blur', outInput);
-}
-//textArea.addEventListener('click', activeLabel);
-//textArea.addEventListener('blur', outInput);
-//functions  
+document.addEventListener('DOMContentLoaded', function(){
+    btnMenu.addEventListener('click', openMenu);
+    btnSearch.addEventListener('click', openSearch); 
+    inputSearch.addEventListener('click', clearSearch); 
+    for(i=0;input.length>i;i++){ 
+        input[i].addEventListener('click', activeLabel);
+        input[i].addEventListener('blur', outInput);
+    } 
+    textArea.addEventListener('click', activeLabel);
+    textArea.addEventListener('blur', outInput);   
+})
+
 function createMenuHeader(){ 
     menu.className = 'col-md-4 offset-md-4 col-12 menu hidden text-center'; 
     menu.appendChild(uList); 
@@ -118,6 +120,7 @@ linksHeader.forEach(function(items) {
     link.setAttribute('href', items.href);
     link.setAttribute('target', items.target); 
 });
+
 /*
 linksFooter.forEach(function(items){
 let list = document.createElement('li');
@@ -125,3 +128,52 @@ let link = document.createElement('a');
 });
 */
  
+/* filters */
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("card");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it) 
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+} 
